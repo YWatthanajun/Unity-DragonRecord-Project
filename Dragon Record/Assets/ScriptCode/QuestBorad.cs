e43a997507e5470aa1fca18bf9fab8379194623e
YWatthanajun <ywatthanajun@gmail.com>
2023-12-21 00:40:30 +0700

    21-12-2566_UpdateBorad_01

---
+++ Dragon Record/Assets/ScriptCode/QuestBorad.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestBorad : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public AudioSource audioSource;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                audioSource.PlayOneShot(audioClip);
+                Debug.Log("Clicked on: " + hit.collider.gameObject.name);
+                // Perform desired action when object is clicked
+            }
+        }
+    }
+}
