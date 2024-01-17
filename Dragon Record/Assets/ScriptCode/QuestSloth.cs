e06496c81d2e89ed86022f85b9ce45e1eb6d1625
YWatthanajun <ywatthanajun@gmail.com>
2024-01-18 03:20:32 +0700

    Update QuestSloth.cs

--- Dragon Record/Assets/ScriptCode/QuestSloth.cs
+++ Dragon Record/Assets/ScriptCode/QuestSloth.cs
@@ -5,14 +5,12 @@ using UnityEngine.EventSystems;
 
 public class QuestSloth : MonoBehaviour, IDropHandler 
 {
-    public QuestLevel QuestLevel;
 
     public void OnDrop(PointerEventData eventData) {
         Debug.Log("OnDrop");
         if (eventData.pointerDrag != null) //ตรวจสอบว่า มีวัตถุที่ลากจริงรึเปล่า
         {
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition; // ย้ายตำแหน่ง GameObject ไปยังช่อง QuestSloth
-            QuestBoradManager.Instance.Add(QuestLevel);
         }
     }
 }
